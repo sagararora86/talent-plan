48e407f1c3a052416df24642d7bb2a60cde1cf29
Sagar Arora <sagar.ismu@gmail.com>
2023-02-26 19:26:33 +0530

    Adding Logging to Client and Server

--- courses/rust/projects/kvs_3/src/bin/kvs-client.rs
+++ courses/rust/projects/kvs_3/src/bin/kvs-client.rs
@@ -1,8 +1,16 @@
 use std::env::current_dir;
 use clap::{arg, Command};
 use kvs::{KvStore, Result};
+use log::{info, error};
 
 fn main() -> Result<()> {
+
+    stderrlog::new()
+    .module(module_path!())
+    .verbosity(3)
+    .timestamp(stderrlog::Timestamp::Millisecond)
+    .init().unwrap();
+
     let version = env!("CARGO_PKG_VERSION");
     let matches = Command::new("Kvs")
         .version(version)
@@ -14,18 +22,31 @@ fn main() -> Result<()> {
             Command::new("get").args(vec![arg!([KEY]).required(true)]),
             Command::new("rm").args(vec![arg!([KEY]).required(true)]),
         ])
+        .arg(
+            arg!(--addr <ADDR> "Provide IP:PORT"
+            )
+            .required(false)
+            .default_value("127.0.0.1:4000")
+            
+        )
         .get_matches();
 
-    let mut kv_store = KvStore::open(current_dir().unwrap())?;
+    info!("KVS Server version={}", version);
 
+    let address = matches.get_one::<String>("addr").unwrap();
+    info!("Using IP:PORT={} to send command.", address);
+
+    let mut kv_store = KvStore::open(current_dir().unwrap())?;
     match matches.subcommand() {
         Some(("set", args))=> {
             let key = args.value_of("KEY").unwrap();
             let value = args.value_of("VALUE").unwrap();
+            info!("Executing SET Key={}, Value={}", key, value);
             kv_store.set(key.to_string(), value.clone().to_string())?;
         },
         Some(("get", args)) => {
             let key = args.value_of("KEY").unwrap();
+            info!("Executing GET Key={}", key);
             let value = kv_store.get(key.to_string())?;
             match value {
                 Some(x) => println!("{}", x),
@@ -34,6 +55,7 @@ fn main() -> Result<()> {
         },
         Some(("rm", args)) => {
             let key = args.value_of("KEY").unwrap();
+            info!("Executing RM Key={}", key);
             let result = kv_store.remove(key.to_string());
             match result {
                 Err(err) => {
@@ -43,7 +65,10 @@ fn main() -> Result<()> {
                 _ => {},
             }
         },
-        _ => std::process::exit(1),
+        _ => {
+            error!("Unknown Command, Exiting...");
+            std::process::exit(1)
+        }
     }
     Ok(())
 }
--- courses/rust/projects/kvs_3/src/bin/kvs-server.rs
+++ courses/rust/projects/kvs_3/src/bin/kvs-server.rs
@@ -1,14 +1,20 @@
 use std::{net::TcpListener};
 use clap::{Command, arg};
 use kvs::{Result};
+use log::{info};
 
 fn main() -> Result<()> {
+
+    stderrlog::new()
+        .module(module_path!())
+        .verbosity(3)
+        .timestamp(stderrlog::Timestamp::Millisecond)
+        .init().unwrap();
+
     let version = env!("CARGO_PKG_VERSION");
     let matches = Command::new("kvs-server")
     .arg(
-        arg!(--addr <ADDR> "Provide IP:PORT"
-        )
-        // We don't have syntax yet for optional options, so manually calling `required`
+        arg!(--addr <ADDR> "Provide IP:PORT")
         .required(false)
         .default_value("127.0.0.1:4000")
         
@@ -18,11 +24,13 @@ fn main() -> Result<()> {
 
     let address = matches.get_one::<String>("addr").unwrap();
 
+    info!("KVS Server version={}", version);
+    info!("Started Listening on IP:PORT={}", address);
 
     let tcp_listener = TcpListener::bind(address).unwrap();
     for stream in tcp_listener.incoming() {
         let _tcp_stream = stream.unwrap();
-        print!("Connection Established!!")
+        info!("Connected to Client...")
     }
 
     Ok(())
